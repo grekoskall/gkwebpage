505116a2d1619b969b39e99ebfe393501b173c3a
grekoskall <grekoskall@gmail.com>
2023-05-31 00:00:39 +0300

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,8 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-   /*  baseUrl: 'https://gkpage-ts.onrender.com', */
-    baseUrl: 'http://localhost:4500',
+    baseUrl: 'https://gkpage-ts.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
